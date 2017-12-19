4ef1c3a924ba09b88ebf203da57bfa82525077f5
Xavier Pillons <xpillons@microsoft.com>
2017-12-19 12:42:17 +0100

    fix source of params file

--- 03-nodeprep.sh
+++ 03-nodeprep.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$DIR/common.sh"
-source params.tpl
+if [ $# != 1 ]; then
+    echo "Usage: $0 <paramsfile>"
+    exit 1
+fi
+
+source $1
 
 required_envvars container_name storage_account_name
 
