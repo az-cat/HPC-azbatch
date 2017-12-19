7e19ed3187793321e7b691920eecde4de4d1e384
Xavier Pillons <xpillons@microsoft.com>
2017-12-19 17:41:17 +0100

    split command line

--- 03-nodeprep.sh
+++ 03-nodeprep.sh
@@ -10,4 +10,8 @@ source $1
 
 required_envvars container_name storage_account_name
 
-az storage blob upload --account-name $storage_account_name --container $container_name --file $DIR/nodeprep.sh --name nodeprep.sh
+az storage blob upload \
+    --account-name $storage_account_name \
+    --container $container_name \
+    --file $DIR/nodeprep.sh \
+    --name nodeprep.sh
