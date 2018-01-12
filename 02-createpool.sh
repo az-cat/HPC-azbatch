210758b5e12849a730c5c64b7cd7ae6d7c390569
Xavier Pillons <xpillons@microsoft.com>
2018-01-12 13:27:44 +0100

    use json only to create pool

--- 02-createpool.sh
+++ 02-createpool.sh
@@ -39,7 +39,21 @@ echo "generating sas key for container $container_name"
 saskey=$(az storage container generate-sas --policy-name "read" --name ${container_name} --account-name ${storage_account_name} | jq -r '.')
 
 nodeprep_uri="https://${storage_account_name}.blob.core.windows.net/${container_name}/nodeprep.sh?${saskey}"
-jq '.startTask.resourceFiles[0].blobSource=$blob' $DIR/pool-template.json --arg blob "$nodeprep_uri" > ${pool_id}-pool.json
+jq '.id=$poolId | \
+    .vmSize=$vmSize \
+    .virtualMachineConfiguration.nodeAgentSKUId=$node_agent \
+    .virtualMachineConfiguration.imageReference.publisher=$publisher \
+    .virtualMachineConfiguration.imageReference.offer=$offer \
+    .virtualMachineConfiguration.imageReference.sku=$sku \    
+    .startTask.resourceFiles[0].blobSource=$blob' $DIR/pool-template.json \
+    --arg blob "$nodeprep_uri" \
+    --arg poolId "$poolId" \
+    --arg vmSize "$vm_size" \
+    --arg publisher "$(echo $vm_image | cut -d':' -f1)" \
+    --arg offer "$(echo $vm_image | cut -d':' -f2)" \
+    --arg sku "$(echo $vm_image | cut -d':' -f3)" \
+    --arg node_agent "$node_agent" \
+    > ${pool_id}-pool.json
 
 if [ -n "$app_package" ]; then
     jq '.applicationPackageReferences[0].applicationId=$package | .applicationPackageReferences[0].version="latest"' ${pool_id}-pool.json --arg package "$app_package" > tmp.json
@@ -50,15 +64,16 @@ fi
 echo "create pool ${pool_id}"
 az batch pool create \
     --account-name $batch_account \
-    --id $pool_id \
-    --vm-size $vm_size \
-    --image $vm_image \
-    --node-agent-sku-id "$node_agent" \
-    --enable-inter-node-communication
-
-echo "set pool configuration"
-az batch pool set \
-    --account-name $batch_account \
-    --pool-id $pool_id \
+#    --id $pool_id \
+#    --vm-size $vm_size \
+#    --image $vm_image \
+#    --node-agent-sku-id "$node_agent" \
+#    --enable-inter-node-communication \
     --json-file ${pool_id}-pool.json
+
+#echo "set pool configuration"
+#az batch pool set \
+#    --account-name $batch_account \
+#    --pool-id $pool_id \
+#    --json-file ${pool_id}-pool.json
 
