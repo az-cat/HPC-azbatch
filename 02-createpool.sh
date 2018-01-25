b0e564dfac1ea36df024d3163709452231eb325f
Xavier Pillons <xpillons@microsoft.com>
2018-01-25 10:06:48 +0100

    added pool envsettings

--- 02-createpool.sh
+++ 02-createpool.sh
@@ -10,6 +10,8 @@ fi
 source $1
 
 required_envvars pool_id vm_size vm_image node_agent container_name storage_account_name batch_account
+envsettings="./${pool_id}-envsettings.json"
+poolfile=${pool_id}-pool.json
 
 echo "create pool container"
 az storage container create \
@@ -46,37 +48,44 @@ jq '.id=$poolId |
     --arg blob "$nodeprep_uri" \
     --arg poolId "$pool_id" \
     --arg vmSize "$vm_size" \
-    --arg node_agent "$node_agent" > ${pool_id}-pool.json
+    --arg node_agent "$node_agent" > $poolfile
 
 # check for custom Image or Gallery Image
 if [ -n "$image_id" ]; then # Custom Image is specified
-    jq '.virtualMachineConfiguration.imageReference.virtualMachineImageId=$imageId' ${pool_id}-pool.json \
+    jq '.virtualMachineConfiguration.imageReference.virtualMachineImageId=$imageId' $poolfile \
     --arg imageId "$image_id" > tmp.json
 else # Gallery Image
     jq '.virtualMachineConfiguration.imageReference.publisher=$publisher | 
         .virtualMachineConfiguration.imageReference.offer=$offer | 
-        .virtualMachineConfiguration.imageReference.sku=$sku' ${pool_id}-pool.json \
+        .virtualMachineConfiguration.imageReference.sku=$sku' $poolfile \
     --arg publisher "$(echo $vm_image | cut -d':' -f1)" \
     --arg offer "$(echo $vm_image | cut -d':' -f2)" \
     --arg sku "$(echo $vm_image | cut -d':' -f3)" > tmp.json
 fi
-cp tmp.json ${pool_id}-pool.json
+cp tmp.json $poolfile
 rm tmp.json
 
 if [ -n "$app_package" ]; then
-    jq '.applicationPackageReferences[0].applicationId=$package | .applicationPackageReferences[0].version="latest"' ${pool_id}-pool.json --arg package "$app_package" > tmp.json
-    cp tmp.json ${pool_id}-pool.json
+    jq '.applicationPackageReferences[0].applicationId=$package | .applicationPackageReferences[0].version="latest"' $poolfile --arg package "$app_package" > tmp.json
+    cp tmp.json $poolfile
     rm tmp.json
 fi
 
 if [ -n "$pool_vnet" ]; then
-    jq '.networkConfiguration.subnetId=$pool_vnet ' ${pool_id}-pool.json --arg pool_vnet "$pool_vnet" > tmp.json
-    cp tmp.json ${pool_id}-pool.json
+    jq '.networkConfiguration.subnetId=$pool_vnet ' $poolfile --arg pool_vnet "$pool_vnet" > tmp.json
+    cp tmp.json $poolfile
     rm tmp.json
+fi
+
+# if enviroment variable file exists, merge it
+if [ -f $envsettings ]; then
+    jq '.startTask.environmentSettings=$data' $poolfile --argfile data $envsettings > tmp.json
+    cp tmp.json $poolfile
+    rm tmp.json    
 fi
 
 echo "create pool ${pool_id}"
 az batch pool create \
     --account-name $batch_account \
-    --json-file ${pool_id}-pool.json
+    --json-file $poolfile
 
