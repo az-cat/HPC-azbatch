34ddfe7c5bc3642ea2dabbc7aac943bfd23b60f9
Xavier Pillons <xpillons@microsoft.com>
2018-03-15 09:23:15 +0100

    updated metadata version

--- common/linpack_telemetry.sh
+++ common/linpack_telemetry.sh
@@ -2,7 +2,7 @@
 result_file=$1
 
 echo "extracting Linpack results from $result_file"
-compute=$(curl -s -H Metadata:true "http://169.254.169.254/metadata/instance?api-version=2017-08-01" | jq '.compute')
+compute=$(curl -s -H Metadata:true "http://169.254.169.254/metadata/instance?api-version=2017-12-01" | jq '.compute')
 
 hpl_perf=$(cat $result_file | jq -s -R 'split("\n") | map(select(contains("WC00C2R2"))) | map(split(" ") | map(select(. != ""))) | map({"N": .[1],"NB": .[2],"P": .[3],"Q": .[
 4],"duration": .[5],"gflops": .[6]})'  | jq '.[0]')
@@ -12,3 +12,5 @@ jq -n '.Compute=$compute | .Application="linpack" | .Nodes=$nodes | .ppn=$ppn | 
   --argjson hpl_perf "$hpl_perf" \
   --arg nodes $2 \
   --arg ppn $3  > telemetry.json
+
+
